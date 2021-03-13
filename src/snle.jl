62b4ec3d6b11b0fbf442d3f8dfb6a667612a73b3
korbinian90 <korbinian90@gmail.com>
2021-03-13 22:53:08 +0100

    test train maf

--- src/snle.jl
+++ src/snle.jl
@@ -1,15 +1,30 @@
 # from https://arxiv.org/abs/2101.04653, Appendix A4
 function snle(simulator, prior, qψ=1, R=1, N=1)
     posterior = prior
-    D = []
+    D = Data(nothing, nothing)
     for r in 1:R
         θ = sample(posterior, N)
         x = simulate(simulator, θ)
-        push!(D, (θ, x))
+        push!(D, θ, x)
         train!(qψ, D)
         posterior = qψ * prior
     end
     return posterior, qψ
+end
+
+mutable struct Data
+    θ
+    x
+end
+
+function Base.push!(D::Data, θ, x)
+    if isnothing(D.θ)
+        D.θ = θ
+        D.x = x
+    else
+        append!(D.θ, θ)
+        append!(D.x, x)
+    end
 end
 
 struct NN
@@ -27,9 +42,20 @@ function build_maf(x, y)
     return NN(py"maf"(x, y))
 end
 
+function log_prob(p::NN, batch_x, by)
+    m = p.maf
+    py"""
+    def log_prob(batch_x, by):
+        t = torch.tensor(batch_x)
+        c = torch.tensor(by)
+        return $(m).log_prob(t, context=c)
+    """
+    return py"log_prob"(batch_x, by)
+end
+
 function sample(p, N)
     θ = rand(p, (N,)) # Distributions.sampler? MCMC
-    return θ
+    return map(x -> Float32.(x), θ)
 end
 
 function simulate(simulator, θ)
@@ -38,6 +64,21 @@ function simulate(simulator, θ)
 end
 
 function train!(qψ, D)
+    torch = pyimport("torch")
+    data = pyimport("torch.utils").data
+    formatdata(d) = torch.Tensor(permutedims(hcat(d...)))
+    dataset = data.TensorDataset(formatdata(D.θ), formatdata(D.x))
+    train_loader = data.DataLoader(dataset; batch_size=50, drop_last=true)
+
+    optimizer = torch.optim.Adam(qψ.maf.parameters())
+    qψ.maf.train()
+    for batch in train_loader
+        optimizer.zero_grad()
+        log_prob = qψ.maf.log_prob(batch[1], context=batch[2])
+        loss = -torch.mean(log_prob)
+        loss.backward()
+        optimizer.step()
+    end
 end
 
 function Base.:*(qψ, p::Sampleable)
--- test/snle.jl
+++ test/snle.jl
@@ -1,20 +1,3 @@
-@testset "SNLE 1D" begin
-    pθ = Uniform(-2, 2)
-    simulator(θ) = 1.0 .+ θ .+ 0.1 .* randn(size(θ))
-    @show snle(simulator, pθ)
-    @show snle(simulator, pθ, 1, 5, 5)
-    # not finished
-end
-
-@testset "SNLE nD" begin
-    ndim = 3
-    pθ = Product([Uniform.(-2, 2) for _ in 1:ndim])
-    simulator(θ) = 1.0 .+ θ .+ 0.1 .* randn(size(θ))
-    @show snle(simulator, pθ)
-    @show snle(simulator, pθ, 1, 5, 5)
-    # not finished
-end
-
 @testset "PyCall maf test" begin    
     #sbi.__init__()
     try
@@ -25,10 +8,54 @@ end
     sbi.build_maf(ones(5), ones(2))
 end
 
+@testset "SNLE 1D" begin
+    ndim = 1
+    nbatch = 50
+    pθ = Product([Uniform.(-2, 2) for _ in 1:ndim])
+    simulator(θ) = 1 .+ θ .+ randn(eltype(θ), size(θ)) ./ 10
+    qψ = sbi.build_maf(rand(Float32, nbatch, ndim), rand(Float32, nbatch, ndim))
+    snle(simulator, pθ, qψ, 2, nbatch)
+end
+
+@testset "SNLE nD" begin
+    ndim = 10
+    nbatch = 50
+    pθ = Product([Uniform.(-2, 2) for _ in 1:ndim])
+    simulator(θ) = 1 .+ θ .+ randn(eltype(θ), size(θ)) ./ 10
+    qψ = sbi.build_maf(rand(Float32, nbatch, ndim), rand(Float32, nbatch, ndim))
+    snle(simulator, pθ, qψ, 2, nbatch)
+end
+
 @testset "Apply maf" begin
+    ndim = 3
+    nbatch = 50
+    pθ = Product([Uniform.(-2, 2) for _ in 1:ndim])
+    simulator(θ) = 1 .+ θ .+ randn(eltype(θ), size(θ)) ./ 10
+    qψ = sbi.build_maf(rand(Float32, nbatch, ndim), rand(Float32, nbatch, ndim))
+    snle(simulator, pθ, qψ, 2, nbatch)
+end
+
+@testset "tests" begin
+    using PyCall
+    N = 50
     pθ = Uniform(-2, 2)
     simulator(θ) = 1.0 .+ θ .+ 0.1 .* randn(size(θ))
-    nbatch_x, nbatch_y = 2, 2
-    @show qψ = sbi.build_maf(ones(nbatch_x), ones(nbatch_y))
-    @show snle(simulator, pθ, qψ, 2, 2)
+    nbatch_x, nbatch_y = N, N
+    qψ = sbi.build_maf(ones(nbatch_x), ones(nbatch_y))
+   
+    sbi.log_prob(qψ, ones(Float32, (N,1)), ones(Float32, (N,1)))
+
+    D = sbi.Data(ones(nbatch_x,1), ones(nbatch_y,1))
+    torch = pyimport("torch")
+    data = pyimport("torch.utils").data
+    dataset = data.TensorDataset(torch.Tensor(D.θ), torch.Tensor(D.x))
+    train_loader = data.DataLoader(dataset; batch_size=50, drop_last=true)
+    optimizer = torch.optim.Adam(qψ.maf.parameters())
+    for batch in train_loader
+        optimizer.zero_grad()
+        log_prob = qψ.maf.log_prob(batch[1], context=batch[2])
+        loss = -torch.mean(log_prob)
+        loss.backward()
+        optimizer.step()
+    end
 end
